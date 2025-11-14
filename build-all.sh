f60c9914101e26eef52c09d68b3a243801049538
LouiseYLi <louiseli.van@gmail.com>
2025-11-13 19:36:23 -0800

    project setup

---
+++ build-all.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Exit immediately if any command fails
+set -e
+
+# BUILD CLIENT
+echo "===BUILD CLIENT==="
+
+echo "Running rustfmt..."
+cd client/
+cargo fmt
+
+echo "Running clippy..."
+cargo clippy --all-targets --all-features -- -Dwarnings
+
+echo "Building client..."
+cargo build
+
+echo "Client successfully built."
+
+# BUILD SERVER
+echo "===BUILD SERVER==="
+
+echo "Running rustfmt..."
+cd ../server/
+cargo fmt
+
+echo "Running clippy..."
+cargo clippy --all-targets --all-features -- -Dwarnings
+
+echo "Building server..."
+cargo build
+
+echo "Server successfully built."
+
+# BUILD PROXY
+echo "===BUILD PROXY==="
+
+echo "Running rustfmt..."
+cd ../proxy/
+cargo fmt
+
+echo "Running clippy..."
+cargo clippy --all-targets --all-features -- -Dwarnings
+
+echo "Building proxy..."
+cargo build
+
+echo "Proxy successfully built."
